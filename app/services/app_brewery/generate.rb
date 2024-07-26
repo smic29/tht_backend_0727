681d601e7798d1ac518745f36de4f660239f5cdd
smic29 <sibulo1991@gmail.com>
2024-07-27 01:15:41 +0800

    Added file generation for rails console use

--- app/services/app_brewery/generate.rb
+++ app/services/app_brewery/generate.rb
@@ -8,4 +8,20 @@ class AppBrewery::Generate
 
     response.data
   end
+
+  def console_task(num, format)
+    return puts "invalid format" unless ['json', 'csv'].include?(format)
+
+    response = @client.get(num, format)
+
+    File.open("response.#{format}", 'w') do |file|
+      if format == 'json'
+        file.write(response.data.to_json)
+      else
+        file.write(response.data)
+      end
+    end
+
+    puts "#{format} file generated"
+  end
 end
